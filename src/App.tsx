34bf621e6db6738ef6817020543de5e467d781a8
tps <schmidt.tps@gmail.com>
2023-09-09 13:13:33 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { IsSok } from './IsSok/IsSok'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <IsSok />
+    </>
+  )
+}
+
+export default App
---
+++ src/IsSok/IsSok.tsx
@@ -0,0 +1,9 @@
+
+
+export function IsSok() {
+    return (
+        <div>
+            <h1>IsSok</h1>
+        </div>
+    )
+}
